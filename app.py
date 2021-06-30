290cc50b159162ecd7f4919fac8639dc35918bb5
AndyMalonda <83769563+AndyMalonda@users.noreply.github.com>
2021-06-30 18:49:28 +0200

    30/06 update
    Adding new things:
    - More html content
    - Lateral nav bar during scrolling
    - Make it responsive

---
+++ app.py
@@ -0,0 +1,11 @@
+print("Bonjour")
+
+nom = input("Quel est ton nom ? ")
+
+print("Enchanté " + nom)
+
+if len(nom) < 8:
+    print("C'est sympa comme prénom")
+else:
+    print("Wow c'est un long prénom")
+
--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
-function myFunction() {
+const menuBox = document.querySelector(".menu");
+
+let lWidth = window.innerWidth;
+console.log(lWidth);
+
+// déclenche la vanne de la matrice dans la page Dev
+function matriceTheme() {
     var element = document.body;
+    var element2 = document.getElementById('textematrice');
     element.classList.toggle("matrice");
+    element2.style.visibility = "visible";
+    menuBox.classList.add("menumatrice");
+    document.getElementById("buttonmatrix").innerHTML = "Voilà c'est tout";
  }
+
+ // menu flottant quand on scroll
+ window.addEventListener("scroll", () => {
+    if (lWidth > 1200 && window.scrollY >= 300) {
+        menuBox.classList.add("menuNav")
+    } else {
+        menuBox.classList.remove("menuNav")
+    }
+ })
